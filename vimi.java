dd7f1129127ecbf6a01750fdbbc7374d872bee9a
siva rushinadh <karanamrushi834@gmail.com>
2026-01-05 22:07:13 +0530

    Add files via upload

---
+++ vimi.java
@@ -0,0 +1,12 @@
+public class vimi {
+    static void main(String... args){
+        StringBuilder bim=new StringBuilder("hello nadh");
+        String bom="komni nadh nsofdods";
+        System.out.println(bim.substring(3));
+        System.out.println(bim.capacity());
+        System.out.println(bim.codePointAt(2));
+        System.out.println(bom.replaceAll(" ", ""));
+        System.out.println(bom.repeat(5));
+    }
+    
+}
