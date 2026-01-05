39b21abb16991deae60756a79bc19092618bde1b
siva rushinadh <karanamrushi834@gmail.com>
2026-01-05 22:06:24 +0530

    Add files via upload

---
+++ srtr5.java
@@ -0,0 +1,37 @@
+
+class srtr5{
+    static void main(String[] args){
+StringBuilder s=new StringBuilder("hlo Rushi nadh");
+StringBuilder j=new StringBuilder("hlo Rshi adh");
+s.charAt(0);
+Boolean d=s.isEmpty();
+
+s.compareTo(j);
+s.toString();
+System.out.println(s.chars());
+System.out.println(s);
+System.out.println(d);
+
+System.out.println(s.compareTo(j));
+System.out.println(s.substring(5,9));
+String f="siva hlo nadhhhhh hjifoldnafjldm khjdsfknkdskds";
+System.out.println(f.contains(s));
+System.out.println(f.formatted(s));
+System.out.println(f.toUpperCase());
+System.out.println(f.trim());
+System.out.println(f.substring(7));
+System.out.println(f.subSequence(4, 9));
+System.out.println(f.charAt(5));
+System.out.println(f.replace(" ",""));
+System.out.println(s.getClass());
+System.out.println(f.getClass());
+System.out.println(s.chars());
+System.out.println(s.delete(1,5));
+System.out.println(f.charAt(12));
+System.out.println(s);
+System.out.println(s.substring(5,10));
+
+
+
+    }
+}
