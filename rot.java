36579c230eb5c065e6495c8d30caaedf33d7b6fb
Siva Rushinadh <23a31a42b0@Pragati.ac.in>
2026-01-09 21:11:29 +0530

    Add files via upload

--- rot.java
+++ rot.java
@@ -1,10 +1,22 @@
 public class rot {
+    static Boolean pom(String a,String  b){
+        String c=a+b;
+        String a2=a+a;
+        if (a2.contains(b)== true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     public static void main(String[] args) {
         
     
         String d="abcd";
         String b="cdab";
         System.out.println(d.length()==b.length());
+       System.out.println( d.substring(1,3 ));
+       System.out.println(pom(d,b));
     }
     
 }
