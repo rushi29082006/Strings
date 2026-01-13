55088e908c37e94c9c84c5c798324ab8f687e68a
rushi <karanamrushi834@gmail.com>
2026-01-13 19:57:46 +0530

    class nad objects

---
+++ kjl.java
@@ -0,0 +1,22 @@
+public class kjl {
+    String name;
+    int age;
+    float wt;
+    kjl(){
+        name="rushi";
+    }
+    void dom(){
+        System.out.println( age);
+    }
+    static void main(String... args){
+        kjl i=new kjl();
+        
+        i.age=28;
+        i.wt=20.4f;
+        System.out.println(i.name+" "+i.age+""+i.wt);
+        i.dom();
+
+    
+    }
+    
+}
