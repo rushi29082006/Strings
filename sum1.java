44348286f87501494c72a202f0fc2ea8adb6d5df
Siva Rushinadh <23a31a42b0@Pragati.ac.in>
2026-01-17 22:30:20 +0530

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,11 @@
+public class sum1 {
+    static void main(String... args){
+        String name="rushi",city="tuni";
+        int age=29;
+        System.out.println("my name is "+name+ " and age is"+age+" i am living in the"+city);
+        
+
+
+    }
+    
+}
