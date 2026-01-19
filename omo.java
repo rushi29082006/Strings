ddbfc60202a2e74ecbb73577fb4f3f0ca7c839cd
rushi <karanamrushi834@gmail.com>
2026-01-19 16:42:21 +0530

    this is a msg

---
+++ omo.java
@@ -0,0 +1,16 @@
+public class omo {
+    String laptop;
+    int model_year;
+    omo(String Laptop,int model_yrar ){
+        laptop=Laptop;
+        model_year=model_yrar;
+
+
+    }
+    
+        static void main(String[] args){
+            omo hi=new omo("lenovo",2056);
+    
+    System.out.println(hi.laptop);
+        } 
+}
